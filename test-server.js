a1cbea4d43f536bf96ede5fdb0f5f37f5c9fc877
Yufan Wang <yufansmail@gmail.com>
2018-02-06 16:13:25 -0800

    Handles GET and POST requests

--- test-server.js
+++ test-server.js
@@ -1,10 +1,21 @@
 var http = require('http');
 var PORT = 8080;
-var ip = '127.0.0.1';
+var messages = [];
 
 var server = http.createServer(function(request, response) {
   console.log('request got ' + request.method + ' ' + request.url);
+  if (request.method === 'POST') {
+    var body = '';
+    request.on('data', function(chunk) {
+      body += chunk;
+    });
+    request.on('end', function() {
+      messages.push(JSON.parse(body));
+    });
+  } else if (request.method === 'GET') {
+    response.end('Got your request!');
+  }
   response.end('hi');
 });
 
-server.listen(PORT, ip);
+server.listen(PORT);
